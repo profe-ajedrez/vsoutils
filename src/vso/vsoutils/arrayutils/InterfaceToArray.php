82f983550ea95fda62336304bd665514361eb07d
profe-ajedrez <chess.coach.ar@gmail.com>
2020-09-22 20:00:32 -0300

    added interface InterfaceToArray with method sign toArray

---
+++ src/vso/vsoutils/arrayutils/InterfaceToArray.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace vso\vsoutils\arrayutils;
+
+interface InterfaceToArray
+{
+    /**
+     * toArray
+     *
+     * @return array
+     */
+    public function toArray() : array;
+}
